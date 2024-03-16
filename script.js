10af3629edb1ff9cdefd89e8fb03ca63ccc53381
gazemyshoes <mathewsh.236@gmail.com>
2024-03-15 22:33:52 -0500

    Actualiza el proyecto

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-console.log('Hola Mundo');
-
-
+let botonEncriptar = document.querySelector(".btn-encriptar");
+let botonDesencriptar = document.querySelector(".btn-desencriptar");
+let botonLimpiar = document.querySelector(".btn-limpiar");
+let fraseEncriptada = document.querySelector(".text-area");
+let munieco = document.querySelector(".contenedormunieco");
+let parrafo = document.querySelector(".parrafo");
+let resultado = document.querySelector(".texto-salida");
 
 let matriz_code = [
     ["e", "enter"],
@@ -10,18 +14,33 @@ let matriz_code = [
     ["u", "ufat"],
 ];
 
+botonEncriptar.onclick = encriptar;
+botonLimpiar.onclick = limpiarArea;
+
+
+
+function limpiarArea() {
+    document.getElementsByClassName("text-area")[0].value = "";
+}
 
 
 
 
-function encriptar(fraseEncriptada){
+function encriptar (fraseEncriptada){
     for (let i = 0; i < matriz_code.length; i++) {
         if(fraseEncriptada.includes(matriz_code[i][0])) {
             fraseEncriptada = fraseEncriptada.replaceAll(
                 matriz_code[i][0],
-                matriz_code[0][i]
+                matriz_code[i][1]
             );
         }
     }
     return fraseEncriptada;
+    
 }
+
+
+
+
+
+
